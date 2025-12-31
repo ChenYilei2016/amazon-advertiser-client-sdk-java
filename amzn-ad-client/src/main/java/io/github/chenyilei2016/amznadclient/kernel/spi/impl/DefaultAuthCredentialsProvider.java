08bab88399f353f16547370c7111d587578e7f48
陈忆磊 <705029004@qq.com>
2025-12-31 15:17:46 +0800

    refactor

--- amzn-ad-client/src/main/java/io/github/chenyilei2016/amznadclient/kernel/spi/impl/DefaultAuthCredentialsProvider.java
+++ amzn-ad-client/src/main/java/io/github/chenyilei2016/amznadclient/kernel/spi/impl/DefaultAuthCredentialsProvider.java
@@ -4,24 +4,25 @@ import io.github.chenyilei2016.amznadclient.kernel.core.AmznAdAuthCredentialsRes
 import io.github.chenyilei2016.amznadclient.kernel.exceptions.AmznApiException;
 import io.github.chenyilei2016.amznadclient.kernel.spi.IAuthCredentialsProvider;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * Amazon广告认证凭证提供者默认实现
- * 
+ *
  * <p>这是一个空实现,抛出异常提示用户需要提供自己的实现。
  * 外部系统应该通过Spring依赖注入替换此默认实现。
- * 
+ *
  * <p>使用示例:
  * <pre>{@code
  * @Component
  * public class MyAuthCredentialsProvider implements IAuthCredentialsProvider {
- *     
+ *
  *     @Value("${amazon.ad.client-id}")
  *     private String clientId;
- *     
+ *
  *     @Value("${amazon.ad.client-secret}")
  *     private String clientSecret;
- *     
+ *
  *     @Override
  *     public AmznAdAuthCredentialsResponse getAuthCredentialsByProfileId(String profileId) {
  *         // 可以根据profileId返回不同的credentials,或者返回统一的credentials
@@ -32,20 +33,21 @@ import lombok.extern.slf4j.Slf4j;
  *     }
  * }
  * }</pre>
- * 
+ *
  * @author chenyilei
- * @since 2025/12/31
  * @see IAuthCredentialsProvider
+ * @since 2025/12/31
  */
 @Slf4j
-public class DefaultAuthCredentialsProvider implements IAuthCredentialsProvider {
-    
+public class DefaultAuthCredentialsProvider implements IAuthCredentialsProvider, InitializingBean {
+
     @Override
     public AmznAdAuthCredentialsResponse getAuthCredentialsByProfileId(String profileId) {
-        log.error("使用了默认的AuthCredentialsProvider实现,profileId: {}", profileId);
-        throw AmznApiException.createBizException(
-            "未提供AuthCredentialsProvider实现! 请实现IAuthCredentialsProvider接口并注入到Spring容器中。profileId: {}", 
-            profileId
-        );
+        return null;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        throw AmznApiException.createBizException("未提供AuthCredentialsProvider实现! 请实现IAuthCredentialsProvider接口并注入到Spring容器中");
     }
 }
--- amzn-ad-client/src/main/java/io/github/chenyilei2016/amznadclient/kernel/spi/impl/DefaultProfileDetailMetaProvider.java
+++ amzn-ad-client/src/main/java/io/github/chenyilei2016/amznadclient/kernel/spi/impl/DefaultProfileDetailMetaProvider.java
@@ -4,26 +4,27 @@ import io.github.chenyilei2016.amznadclient.kernel.core.ProfileDetailMetaRespons
 import io.github.chenyilei2016.amznadclient.kernel.exceptions.AmznApiException;
 import io.github.chenyilei2016.amznadclient.kernel.spi.IProfileDetailMetaProvider;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * Profile详情数据提供者默认实现
- * 
+ *
  * <p>这是一个空实现,抛出异常提示用户需要提供自己的实现。
  * 外部系统应该通过Spring依赖注入替换此默认实现。
- * 
+ *
  * <p>使用示例:
  * <pre>{@code
  * @Component
  * public class MyProfileDetailMetaProvider implements IProfileDetailMetaProvider {
- *     
+ *
  *     @Autowired
  *     private ProfileRepository profileRepository;
- *     
+ *
  *     @Override
  *     public ProfileDetailMetaResponse getProfileDetailMetaByProfileId(String profileId) {
  *         // 从数据库查询
  *         ProfileEntity entity = profileRepository.findByProfileId(profileId);
- *         
+ *
  *         ProfileDetailMetaResponse response = new ProfileDetailMetaResponse();
  *         response.setProfileId(entity.getProfileId());
  *         response.setAdvRefreshToken(entity.getRefreshToken());
@@ -34,21 +35,22 @@ import lombok.extern.slf4j.Slf4j;
  *     }
  * }
  * }</pre>
- * 
+ *
  * @author chenyilei
- * @since 2025/12/31
  * @see IProfileDetailMetaProvider
+ * @since 2025/12/31
  */
 @Slf4j
-public class DefaultProfileDetailMetaProvider implements IProfileDetailMetaProvider {
-    
+public class DefaultProfileDetailMetaProvider implements IProfileDetailMetaProvider, InitializingBean {
+
     @Override
     public ProfileDetailMetaResponse getProfileDetailMetaByProfileId(String profileId) {
-        log.error("使用了默认的ProfileDetailMetaProvider实现,profileId: {}", profileId);
-        throw AmznApiException.createBizException(
-            "未提供ProfileDetailMetaProvider实现! 请实现IProfileDetailMetaProvider接口并注入到Spring容器中。profileId: {}", 
-            profileId
-        );
+        return null;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        throw AmznApiException.createBizException("未提供 IProfileDetailMetaProvider 实现! 请实现IAuthCredentialsProvider接口并注入到Spring容器中");
     }
 
 }
